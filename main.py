7834c5ec88952fc908fbce062f76e9fa89fad4d7
spfave <sebpfave@gmail.com>
2020-12-25 16:54:48 -0500

    Add exercise details to spreadsheet tracker

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import requests
+from datetime import datetime
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -14,11 +15,11 @@ AGE = 31
 
 
 # Main
-exercise = input("What did you do for exercise today? ")
+exercise_input = input("What did you do for exercise today? ")
 
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 exercise_params = {
-    "query": exercise,
+    "query": exercise_input,
     "gender": GENDER,
     "weight_kg": WEIGHT_KG,
     "height_cm": HEIGHT_CM,
@@ -30,4 +31,30 @@ headers = {
 }
 
 response = requests.post(url=exercise_endpoint,json=exercise_params, headers=headers)
-print(response.json())
+exercise_data = response.json()["exercises"]
+# print(exercise_data)
+
+date_time = datetime.now()
+date_format = date_time.strftime("%m/%d/%Y")
+time_format = date_time.strftime("%X")
+
+
+workout_spreadsheet_username = os.getenv("SHEETLY_USERNAME")
+workout_spreadsheet_project_name = "workoutTracker"
+workout_spreadsheet_sheet_name = "workouts"
+workout_spreadsheet_endpoint = f"https://api.sheety.co/{workout_spreadsheet_username}/{workout_spreadsheet_project_name}/{workout_spreadsheet_sheet_name}"
+
+for exercise in exercise_data:
+
+    exercise_params = {
+        "workout": {
+            "date": date_format,
+            "time": time_format,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"],
+        }
+    }
+
+    sheet_response = requests.post(url=workout_spreadsheet_endpoint, json=exercise_params)
+    # print(sheet_response)
